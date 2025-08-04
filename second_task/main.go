4128a97d6e785f9f3b3fc35bc2c6e2fe8acceeeb
Gleb <dawgkek@mail.ru>
2025-08-04 17:35:21 +0300

    added two task

---
+++ second_task/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Merge принимает два read-only канала и возвращает выходной канал,
+// в который последовательно (в любом порядке) будут отправлены все значения
+// из обоих входных каналов.
+//
+// Выходной канал должен быть закрыт после того, как оба входных канала закроются.
+// Merge не должен закрывать входные каналы
+//
+// Для проверки решения запустите тесты: go test -v
+func Merge(ch1, ch2 <-chan int) <-chan int {
+	// TODO: реализуйте эту функцию
+
+	result := make(chan int)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for val := range ch1 {
+			result <- val
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for val := range ch2 {
+			result <- val
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	return result
+}
+
+func main() {
+	a := make(chan int)
+	b := make(chan int)
+
+	go func() {
+		defer close(a)
+		a <- 4
+		a <- 1
+	}()
+
+	go func() {
+		defer close(b)
+		b <- 2
+		b <- 4
+	}()
+
+	for v := range Merge(a, b) {
+		fmt.Println(v)
+	}
+}
